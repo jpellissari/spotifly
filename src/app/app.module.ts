85a6e063a7245d1d9bf2c3364818c35fc8c49fe7
João Henrique Moretti Pellissari <pellissari.joao@gmail.com>
2023-01-15 18:19:26 -0300

    feat: add HttpClientModule to make api calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ArtistsModule } from './artists/artists.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    ArtistsModule,
+    AlbumsModule,
     BrowserAnimationsModule,
     MatIconModule,
+    HttpClientModule,
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
